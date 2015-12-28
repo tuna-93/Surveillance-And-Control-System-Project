d2e6236ad557e172709d7d3cf34c054b82b9f3bc
동원 이 <i_chamchi@naver.com>
2015-12-28 15:05:39 +0900

    github 사용 시작

--- Test1/Form1.cs
+++ Test1/Form1.cs
@@ -19,9 +19,7 @@ namespace Test1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //test str 
-            MessageBox.Show("노수찬");
-            //저거 맞나 확인해봐바
+            MessageBox.Show("2015. 12. 28 GitHub 생성.");
         }
     }
 }
