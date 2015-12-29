c46b2be13a7682474d8b50b332d2a59b5d70e0b8
KRODU <nscksp@gmail.com>
2015-12-29 20:10:07 +0900

    New Project

---
+++ SCS_Server/NetCommon.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+
+namespace NetCommon
+{
+    /// <summary>
+    /// 네트워크를 통해 전송될 객체입니다.
+    /// </summary>
+    [Serializable]
+    class SendingObj : ISerializable
+    {
+        /// <summary>
+        /// 예외 메시지를 가져옵니다. 없을 경우 null입니다.
+        /// </summary>
+        public string ExceptionMessage { get; private set; }
+
+        /// <summary>
+        /// 전송 유형을 나타냅니다.
+        /// </summary>
+        public SendingType SendingType { get; private set; }
+
+        /// <summary>
+        /// 같이 전송되는 추가 정보 객체입니다. 없을 경우 null입니다.
+        /// </summary>
+        public object TagData { get; private set; }
+
+        /// <summary>
+        /// 전송 유형과 같이 전송할 객체, 서버 또는 클라이언트에서 발생한 예외 메시지를 지정하여 초기화합니다.
+        /// </summary>
+        /// <param name="sendingType">전송 유형입니다.</param>
+        /// <param name="tagData">같이 전송할 객체입니다.</param>
+        /// <param name="exceptionStr">발생한 예외 메시지입니다.</param>
+        public SendingObj(SendingType sendingType, object tagData = null, string exceptionStr = null)
+        {
+            SendingType = sendingType;
+            TagData = tagData;
+            ExceptionMessage = exceptionStr;
+        }
+
+        /// <summary>
+        /// ISerializable를 통해 구현하는 직렬화입니다.
+        /// </summary>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("ExceptionMessage", ExceptionMessage);
+            info.AddValue("SendingType", SendingType);
+            info.AddValue("TagData", TagData);
+        }
+
+        /// <summary>
+        /// 지정된 Stream에 현재 객체를 직렬화하여 씁니다.
+        /// </summary>
+        /// <param name="serializeStream">직렬화에 쓸 스트림입니다.</param>
+        public void Serialize(Stream serializeStream)
+        {
+            if (serializeStream == null)
+                throw new ArgumentNullException();
+
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(serializeStream, this); // 직렬화합니다.
+        }
+
+        /// <summary>
+        /// 지정된 Stream에서 읽어들여 역직렬화합니다.
+        /// </summary>
+        /// <param name="deserializeStream">읽어들일 스트림입니다.</param>
+        public static SendingObj Deserialize(Stream deserializeStream)
+        {
+            if (deserializeStream == null)
+                throw new ArgumentNullException();
+
+            BinaryFormatter bf = new BinaryFormatter();
+            return bf.Deserialize(deserializeStream) as SendingObj; // 역직렬화한 뒤 SendingObj로 캐스팅하여 반환합니다.
+        }
+    }
+
+    /// <summary>
+    /// 전송 유형을 나타납니다.
+    /// </summary>
+    enum SendingType
+    {
+        // --- 서버에서 클라이언트로 전송됩니다. ---
+
+        /// <summary>
+        /// 서버에서 클라이언트로 전송되는 화면에 표시될 메시지 입니다.
+        /// </summary>
+        SToC_SpreadMessage,
+
+        // --- 클라이언트에서 서버로 전송됩니다. ---
+
+        /// <summary>
+        /// 클라이언트에서 서버로 전송되는 메시지 전달 요청입니다.
+        /// </summary>
+        CToS_SpreadMessageReq
+    }
+}
---
+++ SCS_Server/NetServer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSTest
+{
+    class NetListenerServer : IDisposable
+    {
+        /// <summary>
+        /// 서버로 연결할 때 필요한 포트 번호 필드입니다.
+        /// </summary>
+        private readonly int int_portNumber;
+
+        /// <summary>
+        /// 서버로 연결할 때 필요한 포트 번호를 반환합니다.
+        /// </summary>
+        public int Port { get { return int_portNumber; } }
+
+        /// <summary>
+        /// 포트 번호를 지정해 서버 객체를 생성합니다.
+        /// </summary>
+        /// <param name="port">열 포트번호입니다.</param>
+        public NetListenerServer(int port)
+        {
+            int_portNumber = port;
+        }
+
+        /// <summary>
+        /// 지정한 포트번호를 열어, 연결 수신을 시작합니다.
+        /// </summary>
+        public void PortOpen()
+        {
+
+        }
+
+        /// <summary>
+        /// 열었던 포트를 닫고 모든 연결을 닫습니다.
+        /// </summary>
+        public void PortClose()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            PortClose();
+        }
+    }
+}
